6f6d0f790139a1b629237d6bc5f5f117a88e2be0
angesetgebresilassie-collab <angesetgebresilassie@gmail.com>
2026-09-10 09:49:16 +0300

    Upgrade compileSdk and targetSdk to version 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk { version = release(37) { minorApiLevel = 1 } }
 
   defaultConfig {
     applicationId = "com.aistudio.musicstream.kxuvqp"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 37
     versionCode = 1
     versionName = "1.0"
 
